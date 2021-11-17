2311adc1798590304d6465fb521ebca2faaab1d3
Mjnorvor <jamnorvor@st.ug.edu.gh>
2021-11-17 12:34:44 +0000

    New App.js
    More changes

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ImageBackground,TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from "./screens/Login";
+import Details from './screens/Details';
+import SignUp from './screens/SignUp';
+import specs from './screens/specs';
 
 
 
